44cee75fcb6ffcae9273a5497f9a35109b52820d
dekatotoro <nekoganeru@gmail.com>
2015-12-14 14:15:56 +0900

    Initial Commit

---
+++ PlaybackButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlaybackButton
+//
+//  Created by Yuji Hato on 12/14/15.
+//  Copyright © 2015 dekatotoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
